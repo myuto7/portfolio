2898571a428b6c40dbd09dbec575f192aa2b1f92
mizuno <31834@toyota.kosen-ac.jp>
2022-09-09 23:59:45 +0900

    Add: Header

---
+++ compornents/Header.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next'
+
+const Header: NextPage = () => {
+    return (
+        <>
+            <p>about</p>
+            <p>work</p>
+            <p>blog</p>
+        </>
+    )
+}
+
+export default Header
