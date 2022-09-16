143fd810a76a6c709c86e133e19eeedbf7572882
mizuno <31834@toyota.kosen-ac.jp>
2022-09-17 00:34:01 +0900

    Apply work style

--- compornents/Work.tsx
+++ compornents/Work.tsx
@@ -1,11 +1,12 @@
 import { WorkData } from "./dataset";
+import styles from "../styles/Home.module.css";
 
 const Work: React.FC<WorkData> = (props) => {
     return (
-        <div>
+        <div className={styles.work}>
             <p>{props.title}</p>
             <p>{props.explain}</p>
-            <a href={props.url}></a>
+            <a className={styles.link} href={props.url}></a>
         </div>
     )
 }
