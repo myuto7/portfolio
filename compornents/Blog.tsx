29c5f8375f42092cfff5b1445bcb43a103270c43
mizuno <31834@toyota.kosen-ac.jp>
2022-10-02 02:11:47 +0900

    Fix: aspect ratio

--- compornents/Blog.tsx
+++ compornents/Blog.tsx
@@ -5,8 +5,8 @@ import Image from "next/image";
 const Blog: React.FC<BlogData> = (props) => {
     return (
         <div className={styles.blog}>
+            <Image src={props.image} alt="noimage" layout="fill" className={styles.blogImage} />
             <p>{props.title}</p>
-            <Image src={props.image} alt="noimage" layout="fill" className={styles.image} />
             <a className={styles.link} href={props.url}></a>
         </div>
     )
